cd717e366852a0159006c0e97db4e859ef38f869
Deva Das <devadas3344@gmail.com>
2025-12-18 22:50:22 +0530

    new-one

---
+++ script.js
@@ -0,0 +1,29 @@
+let homeSection = document.getElementById("home");
+let homeButton = document.getElementById("homeBtn");
+
+let aboutSection = document.getElementById("about");
+let aboutButton = document.getElementById("aboutBtn");
+
+let contentSection = document.getElementById("content");
+let contentButton = document.getElementById("contentBtn");
+
+const hideAll  = () => {
+    homeSection.classList.add("display-off");
+    aboutSection.classList.add("display-off");
+    contentSection.classList.add("display-off");
+}
+
+homeButton.onclick = () => {
+    hideAll();
+    homeSection.classList.remove("display-off");
+};
+
+aboutButton.onclick = () => {
+    hideAll();
+    aboutSection.classList.remove("display-off");
+};
+
+contentButton.onclick = () => {
+    hideAll();
+    contentSection.classList.remove("display-off");
+};
